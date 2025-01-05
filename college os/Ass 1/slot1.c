05490c0f5b50f63ea709f208909bf403976881e0
Demo10023 <155277833+Demo10023@users.noreply.github.com>
2025-01-05 16:42:44 +0530

    Update slot1.c

--- college os/Ass 1/slot1.c
+++ college os/Ass 1/slot1.c
@@ -2,109 +2,130 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
-// Function to count characters, words, or lines in a file
-void count_command(char option, const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        perror("Error opening file");
+#define MAX_LINE 1024
+
+void count_file(char option, char *filename)
+{
+    FILE *fp = fopen(filename,"r");
+
+    if(fp==NULL)
+    {
+        perror("File not Found !");
         return;
     }
 
-    int character_count = 0, word_count = 0, line_count = 0;
+    int count = 0;
     char ch;
-    int in_word = 0;
 
-    while ((ch = fgetc(file)) != EOF) {
-        character_count++;
-
-        if (ch == '\n') {
-            line_count++;
+    switch(option) 
+    {
+    case 'c':
+        while((ch =fgetc(fp)) != EOF)
+        {
+            count++;
         }
-        if (ch == ' ' || ch == '\n' || ch == '\t') {
-            in_word = 0;
-        } else if (in_word == 0) {
-            in_word = 1;
-            word_count++;
-        }
-    }
-    fclose(file);
+        break;
 
-    switch (option) {
-        case 'c':
-            printf("Character Count: %d\n", character_count);
-            break;
-        case 'w':
-            printf("Word Count: %d\n", word_count);
-            break;
-        case 'l':
-            printf("Line Count: %d\n", line_count);
-            break;
-        default:
-            printf("Invalid option. Use 'c' for characters, 'w' for words, 'l' for lines.\n");
+    case 'w':
+        while(fscanf(fp,"%*s") != EOF)
+        {
+            count++;
+        }
+        break;
+
+    case 'l':
+        while((ch=fgetc(fp)) != EOF)
+        {
+            if(ch=='\n')
+            {
+                count++;
+            }
+        }
+        break;
+
+    default :
+        perror("Invalid Command for Count !");
+        fclose(fp);
+        return;
     }
+
+    printf("Count : %d\n",count);
+    fclose(fp);
 }
 
-// Execute external shell commands
-void execute_external_command(char **args) {
-    pid_t pid = fork();
-    if (pid == 0) {
-        // Child process
-        if (execvp(args[0], args) == -1) {
-            perror("Error executing command");
-        }
-        exit(EXIT_FAILURE);
-    } else if (pid > 0) {
-        // Parent process
-        wait(NULL);
-    } else {
-        perror("Fork failed");
-    }
-}
-
-// Parse and execute the command
-void execute_command(char *command) {
-    char *args[10];
-    int i = 0;
+void execute_command(char *command) 
+{
+    char *tokens[MAX_LINE/2 + 1];
     char *token = strtok(command, " ");
-    
-    while (token != NULL) {
-        args[i++] = token;
+    int i=0;
+
+    while(token != NULL)
+    {
+        tokens[i++] = token;
         token = strtok(NULL, " ");
     }
-    args[i] = NULL;
 
-    if (args[0] == NULL) {
+    tokens[i] = NULL;
+
+    if(i==0) 
+    {
         return;
     }
 
-    if (strcmp(args[0], "exit") == 0) {
-        printf("Exiting myshell...\n");
+    if(strcmp(tokens[0],"count") == 0 && i==3)
+    {
+        //tokens[1][0] extracts the first character from the string stored in tokens[1]
+        count_file(tokens[1][0],tokens[2]);
+        
+    }
+    else if(strcmp(tokens[0],"q")==0)
+    {
         exit(0);
     }
+    else
+    {
+        pid_t pid = fork();
 
-    if (strcmp(args[0], "count") == 0 && i == 3) {
-        count_command(args[1][0], args[2]);
-    } else {
-        execute_external_command(args);
+        if(pid == 0) //child process
+        {
+            execvp(tokens[0], tokens);
+            perror("Command not found");
+            exit(1);
+
+        }
+        else if(pid>0) //parent process
+        {
+            wait(NULL);
+        }
+        else //fork failled
+        {
+            perror("Fork Failled !");
+        }
+
     }
+
 }
 
-int main() {
-    char command[100];
+int main() 
+{
+    char command[MAX_LINE];
 
-    while (1) {
+    while(1) 
+    {
         printf("myshell$ ");
-        if (fgets(command, sizeof(command), stdin) == NULL) {
-            perror("Error reading input");
-            continue;
+
+        if(fgets(command,MAX_LINE,stdin)== NULL) 
+        {
+            exit(0);
+        } 
+        else 
+        {
+            command[strcspn(command, "\n")] = 0;
+            execute_command(command);
         }
-        
-        // Remove newline character from input
-        command[strcspn(command, "\n")] = '\0';
-        
-        execute_command(command);
     }
     return 0;
 }
